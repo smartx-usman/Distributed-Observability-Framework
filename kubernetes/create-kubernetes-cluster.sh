b2e8e9f86b43399a17dfdb1ca0549ec6ebf8ee75
smartx-usman <usman@smartx.kr>
2022-01-03 16:56:49 +0100

    Add network time synchronization tool.

--- kubernetes/create-kubernetes-cluster.sh
+++ kubernetes/create-kubernetes-cluster.sh
@@ -13,6 +13,7 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
+    linuxptp \
     lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
