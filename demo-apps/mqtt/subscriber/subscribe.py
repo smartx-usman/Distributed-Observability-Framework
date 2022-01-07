f5dbf575e0f4544ce4e4292fa21406240f77206b
smartx-usman <usman@smartx.kr>
2022-01-07 11:37:18 +0100

    Parameterize values and use logging framework.

--- demo-apps/mqtt/subscriber/subscribe.py
+++ demo-apps/mqtt/subscriber/subscribe.py
@@ -2,37 +2,36 @@
 import os
 import random
 import json
+import logging
 
 from kafka import KafkaProducer
 from paho.mqtt import client as mqtt_client
 
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
 mqtt_broker = os.environ['MQTT_BROKER']
+mqtt_port = int(os.environ['MQTT_BROKER_PORT'])
+mqtt_topic = os.environ['MQTT_TOPIC']
 kafka_broker = os.environ['KAFKA_BROKER']
-mqtt_port = 1883
-mqtt_topic = "mqtt/temperature"
-kafka_topic = "temperature"
+kafka_topic = os.environ['KAFKA_TOPIC']
 kafka_key = "server-room"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
-# username = 'emqx'
-# password = 'public'
 
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print("Connected to MQTT Broker!")
+            logging.info('Connected to MQTT Broker!')
         else:
-            print("Failed to connect, return code %d\n", rc)
+            logging.critical(f'Failed to connect, return code {rc}.')
 
     try:
         client = mqtt_client.Client(client_id)
-        #client.username_pw_set(username, password)
         client.on_connect = on_connect
         client.connect(mqtt_broker, mqtt_port)
     except Exception as ex:
-        print('Exception while connecting MQTT')
-        print(ex)
+        logging.critical('Exception while connecting MQTT.', exc_info=True)
     return client
 
 def connect_kafka_producer(kafka_broker):
@@ -40,8 +39,7 @@ def connect_kafka_producer(kafka_broker):
     try:
       _producer = KafkaProducer(bootstrap_servers=kafka_broker)
     except Exception as ex:
-        print('Exception while connecting Kafka')
-        print(ex)
+        logging.critical('Exception while connecting Kafka.', exc_info=True)
     return _producer
 
 def kafka_publish_message(producer_instance, message):
@@ -58,18 +56,16 @@ def kafka_publish_message(producer_instance, message):
         value_bytes = bytes(message_dump, encoding='utf-8')
         producer_instance.send(kafka_topic, key=key_bytes, value=value_bytes)
         producer_instance.flush()
-        print('Message published successfully.')
+        logging.info('Message published successfully.')
     except Exception as ex:
-        print('Exception in publishing message')
-        print(ex)
+        logging.error('Exception in publishing message.', exc_info=True)
+
 
 def mqtt_subscribe_message(client: mqtt_client, producer):
     def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        logging.debug(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
         message = msg.payload.decode()
-        #message = message.encode()
         kafka_publish_message(producer, message)
-        #producer.send('temperature', value=message)
 
     client.subscribe(mqtt_topic)
     client.on_message = on_message
