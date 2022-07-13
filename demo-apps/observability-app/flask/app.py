e9963e70a435ba1bed01a466412fed37005d526b
smartx-usman <usman@smartx.kr>
2022-07-13 17:10:15 +0200

    Add matrix multiplication. Add sorting algorithms. Enable logging. Add data store option to MySQL.

--- demo-apps/observability-app/flask/app.py
+++ demo-apps/observability-app/flask/app.py
@@ -1,13 +1,18 @@
+import logging
 import subprocess
 import time
 from random import randint
 
+import numpy as np
 from flask import Flask, request, jsonify, abort
+from flask_mysqldb import MySQL
 from opentelemetry import trace
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 resource = Resource(attributes={
     SERVICE_NAME: "observability-service"
@@ -26,6 +31,12 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'mysql.uc1.svc.cluster.local'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'flask'
+
+mysql = MySQL(app)
 
 
 @app.route("/roll")
@@ -69,6 +80,114 @@ def multi_span():
     return 'Finished'
 
 
+@app.route("/matrix-multiplication")
+def matrix_multiply():
+    # Create a new span to track some work
+    start_dt = time.time()
+    start = int(request.args.get('start'))
+    end = int(request.args.get('end'))
+    rows = int(request.args.get('rows'))
+    cols = int(request.args.get('cols'))
+    with tracer.start_as_current_span("matrix-multiply") as parent_span:
+        with tracer.start_as_current_span("matrix-data-op") as child_span1:
+            child_span1.set_attribute('start_range', start)
+            child_span1.set_attribute('end_range', end)
+            matrix_a = np.random.randint(start, end, size=(rows, cols))
+            matrix_b = np.random.randint(start, end, size=(rows, cols))
+
+        # Create a nested span to track nested work
+        with tracer.start_as_current_span("matrix-multiply-op") as child_span2:
+            result = np.matmul(matrix_a, matrix_b)
+            child_span2.set_attribute('rows', rows)
+            child_span2.set_attribute('cols', cols)
+            # the nested span is closed when it's out of scope
+
+        # Now the parent span is the current span again
+
+        end_dt = time.time()
+        latency = end_dt - start_dt
+        parent_span.set_attribute('e2e_latency', latency)
+    return 'Finished.'
+
+
+@app.route("/sorting")
+def sorting():
+    # Create a new span to track some work
+    with tracer.start_as_current_span("sorting") as parent_span:
+        start_dt = time.time()
+        kind = request.args.get('kind')
+        start = int(request.args.get('start'))
+        end = int(request.args.get('end'))
+        size = int(request.args.get('size'))
+
+        data = generate_data(start=start, end=end, size=size)
+        sort_data(data=data, kind=kind)
+
+        # Now the parent span is the current span again
+        end_dt = time.time()
+        latency = end_dt - start_dt
+        parent_span.set_attribute('e2e_latency', latency)
+    return 'Request completed.'
+
+
+def generate_data(start, end, size):
+    with tracer.start_as_current_span("generate-data-op") as child_span1:
+        child_span1.set_attribute('start_range', start)
+        child_span1.set_attribute('end_range', end)
+        child_span1.set_attribute('size', size)
+        data = np.random.randint(start, end, size=size)
+        return data
+
+
+def sort_data(data, kind):
+    with tracer.start_as_current_span("sort-data-op") as child_span2:
+        child_span2.set_attribute('kind', kind)
+        sorted_data = np.sort(data, axis=0, kind=kind)
+        save_data(data=sorted_data, kind=kind)
+
+
+def save_data(data, kind):
+    with tracer.start_as_current_span("save-data-op") as child_span3:
+        # child_span3.set_attribute('start_range', start)
+        # child_span3.set_attribute('end_range', end)
+        server = request.remote_addr
+        client = request.environ.get('HTTP_X_FORWARDED_FOR', request.environ['REMOTE_ADDR'])
+        # client = '127.0.0.1'
+        # Creating a connection cursor
+        cursor = mysql.connection.cursor()
+
+        # Executing SQL Statements
+
+        #try:
+        #    cursor.execute(''' DROP TABLE sorted_data ''')
+        #except Exception as error:
+        #    logging.warning("Table does not exist: {}".format(error))
+
+        try:
+            cursor.execute(
+                ''' CREATE TABLE sorted_data(
+                id INT AUTO_INCREMENT PRIMARY KEY, 
+                server VARCHAR(255),
+                client VARCHAR(255), 
+                algorithm VARCHAR(255),
+                value INT UNSIGNED) ''')
+        except Exception as error:
+            logging.fatal("Table is not created: {}".format(error))
+
+        try:
+            for value in data:
+                cursor.execute(''' INSERT INTO sorted_data(server, client, algorithm, value) VALUES(%s, %s, %s, %s) ''',
+                               (server, client, kind, value))
+        except Exception as error:
+            logging.error("Data is not inserted: {}".format(error))
+
+        # Saving the Actions performed on the DB
+        mysql.connection.commit()
+
+        # Closing the cursor
+        cursor.close()
+
+
 @app.route("/normal_load")
 def normal_load():
     hdd = int(request.args.get('hdd'))
@@ -106,10 +225,12 @@ def normal_load():
 def server_error(error):
     return jsonify(success=False, message=error.description), 500
 
-#app.run((host="0.0.0.0")
-# if __name__ == "__main__":
-#    from waitress import serve
-#    serve(app, host="0.0.0.0", port=5000)
+
+# app.run((host="0.0.0.0")
+if __name__ == "__main__":
+    from waitress import serve
+
+    serve(app, host="0.0.0.0", port=5000)
 
 # curl 'http://127.0.0.1:5000/roll?sides=10&rolls=5'
 # curl 'http://127.0.0.1:5000/multispan'
