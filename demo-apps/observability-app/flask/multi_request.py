16b2a7819fa5c3d66737cd4e7f485a0ef14b5bd6
smartx-usman <usman@smartx.kr>
2022-08-26 12:00:17 +0200

    Parametrize max_workers.

--- demo-apps/observability-app/flask/multi_request.py
+++ demo-apps/observability-app/flask/multi_request.py
@@ -15,7 +15,6 @@ def parse_arguments():
     parser.add_argument('-s', '--size', nargs=1, help='Data size to sort.', required=True)
     parser.add_argument('-l', '--algorithm', nargs=1, help='Sorting algorithm. I.e., merge|quick', required=True)
     parser.add_argument('-r', '--requests', nargs=1, help='Number of requests to send.', required=True)
-    # parser.add_argument('-s', '--start_value', nargs=1, help='Range start value', required=True)
 
     return parser.parse_args(sys.argv[1:])
 
@@ -30,13 +29,17 @@ arguments = parse_arguments()
 url = f'http://{arguments.address[0]}:30101/sorting?namespace={arguments.namespace[0]}&size={arguments.size[0]}&start=1&end=1000&kind={arguments.algorithm[0]}&store={arguments.database[0]}&database=flask'
 print(url)
 
+# list_of_urls = ["http://x.x.x.x:30101/drop_database?namespace=uc1&database=flask"] * 1
+# list_of_urls = ["http://x.x.x.x:30101/create_database?namespace=uc1&database=flask"] * 1
 list_of_urls = [url] * int(arguments.requests[0])
 
+n_threads = len(list_of_urls)
 
-with ThreadPoolExecutor(max_workers=32) as pool:
+with ThreadPoolExecutor(max_workers=n_threads) as pool:
     response_list = list(pool.map(get_url, list_of_urls))
 
 for response in response_list:
     print(response)
 
+# How to run this tool
 # python3 demo-apps/observability-app/flask/multi_request.py -a x.x.x.x -d mysql -s 1000 -l merge -n uc1 -r 2
