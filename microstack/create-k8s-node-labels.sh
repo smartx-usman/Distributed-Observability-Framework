24c68388f5eaf6876a28f88a1a0af97355eab525
smartx-usman <usman@smartx.kr>
2023-04-03 16:18:57 +0200

    Show labels on the nodes.

--- microstack/create-k8s-node-labels.sh
+++ microstack/create-k8s-node-labels.sh
@@ -8,3 +8,4 @@ kubectl label nodes k8s-worker-6 disktype=ssd ostype=normal appstype=user flavor
 kubectl label nodes k8s-worker-7 disktype=ssd ostype=normal appstype=user flavor=small
 kubectl label nodes k8s-worker-8 disktype=ssd ostype=normal appstype=user flavor=tiny
 kubectl label nodes k8s-worker-9 disktype=ssd ostype=normal appstype=user flavor=tiny
+kubectl get no --show-labels
