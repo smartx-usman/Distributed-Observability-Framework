189184cd148717f2d62f6a8f25527065a748657a
smartx-usman <usman@smartx.kr>
2023-05-12 11:02:50 +0200

    Add a separate file for topics listing.

---
+++ tools/metrics-analyzer/topics.py
@@ -0,0 +1,18 @@
+# Define the topics to read from Kafka
+topics = [
+    'telegraf_disk',
+    'telegraf_diskio',
+    'telegraf_mem',
+    'telegraf_cpu',
+    'telegraf_net',
+    'telegraf_system',
+    'telegraf_temp',
+    'telegraf_powerstat_package',
+    'telegraf_kubernetes_pod_container',
+    'telegraf_kubernetes_pod_network',
+    'telegraf_kubernetes_pod_volume',
+    'telegraf_kubernetes_daemonset',
+    'telegraf_kubernetes_deployment',
+    'telegraf_kubernetes_service',
+    'telegraf_kubernetes_statefulset'
+]
