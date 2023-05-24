3868229bd4edb68b46df7177fd5a9a248d2e13b7
smartx-usman <usman@smartx.kr>
2023-05-24 12:57:51 +0200

    Reduce number of vm instances to reduce load.

--- microstack/provision-vm-instances.sh
+++ microstack/provision-vm-instances.sh
@@ -5,10 +5,10 @@ microstack.openstack server create --image  ubuntu22.04 --security-group default
 microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker2 --flavor m1.small --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker3
 microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker3 --flavor m1.medium --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker4
 microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker3 --flavor m1.small --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker5
-microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker3 --flavor m1.small --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker6
-microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker1 --flavor m1.small --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker7
-microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker3 --flavor m2.tiny --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker8
-microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker3 --flavor m2.tiny --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker9
+microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker1 --flavor m1.small --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker6
+microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker3 --flavor m2.tiny --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker7
+#microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker3 --flavor m1.small --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker8
+#microstack.openstack server create --image  ubuntu22.04 --security-group default --availability-zone nova:worker3 --flavor m2.tiny --network c62d9fb5-ebeb-4c19-837c-0882129f5918 worker9
 
 # Create floating ip
 microstack.openstack floating ip create external --floating-ip-address 10.20.20.10
@@ -19,8 +19,8 @@ microstack.openstack floating ip create external --floating-ip-address 10.20.20.
 microstack.openstack floating ip create external --floating-ip-address 10.20.20.15
 microstack.openstack floating ip create external --floating-ip-address 10.20.20.16
 microstack.openstack floating ip create external --floating-ip-address 10.20.20.17
-microstack.openstack floating ip create external --floating-ip-address 10.20.20.18
-microstack.openstack floating ip create external --floating-ip-address 10.20.20.19
+#microstack.openstack floating ip create external --floating-ip-address 10.20.20.18
+#microstack.openstack floating ip create external --floating-ip-address 10.20.20.19
 
 # Assign floating ip
 microstack.openstack server add floating ip master1 10.20.20.10
@@ -31,5 +31,5 @@ microstack.openstack server add floating ip worker4 10.20.20.14
 microstack.openstack server add floating ip worker5 10.20.20.15
 microstack.openstack server add floating ip worker6 10.20.20.16
 microstack.openstack server add floating ip worker7 10.20.20.17
-microstack.openstack server add floating ip worker8 10.20.20.18
-microstack.openstack server add floating ip worker9 10.20.20.19
+#microstack.openstack server add floating ip worker8 10.20.20.18
+#microstack.openstack server add floating ip worker9 10.20.20.19
