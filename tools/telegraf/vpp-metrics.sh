48ab1e49de0a40e4df4c94e8021214f3ccfcdd34
smartx-usman <usman@smartx.kr>
2024-04-04 17:03:41 +0200

    Correctly parse the vpp metrics.

--- tools/telegraf/vpp-metrics.sh
+++ tools/telegraf/vpp-metrics.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 # Set the file path
-OUTPUT_FILE="/opt/vpp-metrics.txt"
+RAW_FILE="/opt/vpp-metrics.txt"
+OUTPUT_FILE="/opt/metrics.txt"
 
 # Create the file
 sudo touch "$OUTPUT_FILE"
+sudo touch "$RAW_FILE"
 
 # Change the owner of the file to the user running the script
 current_user=$(whoami)
 sudo chown "$current_user" "$OUTPUT_FILE"
+sudo chown "$current_user" "$RAW_FILE"
 
 # Run the Linux command and capture the output
 while true
 do
-  sudo vppctl show interface > $OUTPUT_FILE
+  sudo vppctl show interface > $RAW_FILE
+  # Normalize line endings to Unix-style (\n)
+  # This assumes the input file has Windows-style line endings (\r\n)
+  tr -d '\r' < "$RAW_FILE" > "$OUTPUT_FILE"
   sleep 1
 done
