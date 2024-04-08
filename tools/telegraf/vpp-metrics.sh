7748c77727c91816054defde8b15360da2003e7d
smartx-usman <usman@smartx.kr>
2024-04-08 17:41:20 +0200

    Add vpp rate and other related metrics collection logic.

--- tools/telegraf/vpp-metrics.sh
+++ tools/telegraf/vpp-metrics.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 # Set the file path
-RAW_FILE="/opt/vpp-metrics.txt"
-OUTPUT_FILE="/opt/metrics.txt"
+INTERFACE_RAW_FILE="/opt/vpp-interface-metrics.txt"
+INTERFACE_OUTPUT_FILE="/opt/interface_metrics.txt"
+RATE_RAW_FILE="/opt/vpp-rate-metrics.txt"
+RATE_OUTPUT_FILE="/opt/vpp_rate.txt"
 
-# Create the file
-sudo touch "$OUTPUT_FILE"
-sudo touch "$RAW_FILE"
+# Create the output files
+sudo touch "$INTERFACE_OUTPUT_FILE" "$INTERFACE_RAW_FILE" "$RATE_OUTPUT_FILE" "$RATE_RAW_FILE"
 
 # Change the owner of the file to the user running the script
 current_user=$(whoami)
-sudo chown "$current_user" "$OUTPUT_FILE"
-sudo chown "$current_user" "$RAW_FILE"
+sudo chown "$current_user" "$INTERFACE_RAW_FILE"
+sudo chown "$current_user" "$INTERFACE_OUTPUT_FILE"
+sudo chown "$current_user" "$RATE_RAW_FILE"
+sudo chown "$current_user" "$RATE_OUTPUT_FILE"
 
-# Run the Linux command and capture the output
+# Run the Linux commands and capture the output
 while true
 do
-  sudo vppctl show interface > $RAW_FILE
+  sudo vppctl show interface > $INTERFACE_RAW_FILE
+  sudo vppctl show runtime | grep memif0 > $RATE_RAW_FILE
+
   # Normalize line endings to Unix-style (\n)
   # This assumes the input file has Windows-style line endings (\r\n)
-  tr -d '\r' < "$RAW_FILE" > "$OUTPUT_FILE"
-  sleep 1
+  tr -d '\r' < "$INTERFACE_RAW_FILE" > "$INTERFACE_OUTPUT_FILE"
+  tr -d '\r' < "$RATE_RAW_FILE" > "$RATE_OUTPUT_FILE"
+  sleep 5
 done
